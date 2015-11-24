25c2730d6117b5887b91966297e292d322ef314c
Szymon Brandys <szymon.brandys@gmail.com>
2015-11-24 15:09:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Eclipse Fans!');
+  res.end('Hello from master branch!');
 }).listen(port);
